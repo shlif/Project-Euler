66e02fdb1fff2288a6cd824cbecafaf7b170245e
shlif <shlifff@gmail.com>
2013-01-17 11:13:28 +0200

    problem_2

---
+++ problem_2.php
@@ -0,0 +1,24 @@
+<?php
+	function fibonacci($n)
+{
+    $sq5 = sqrt(5);
+    $a = (1 + $sq5) / 2;
+    $b = (1 - $sq5) / 2;
+    return (pow($a, $n) - pow($b, $n)) / $sq5;
+}
+
+for ($n = 1; $n <= 100; $n++) {
+		if (fibonacci($n) < 4000000) {
+			if (fibonacci($n)%2 == 0) {
+				$sum+=fibonacci($n);
+			}
+			
+		}
+    else  {
+    	break;
+    }
+}
+echo($sum);
+
+?>
+
